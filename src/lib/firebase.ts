79e5599b0ee92c1d63481ced591589f283f91086
TLoE419 <tloemizuchizu@gmail.com>
2025-04-30 23:26:28 +0800

    Change Firebase Info to Enviroment Variable

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -6,12 +6,12 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAPFEFvfzdJVdIfow4QzBnKHDqh8r6ibmo",
-  authDomain: "eyewear-shop-aaf59.firebaseapp.com",
-  projectId: "eyewear-shop-aaf59",
-  storageBucket: "eyewear-shop-aaf59.firebasestorage.app",
-  messagingSenderId: "395991295721",
-  appId: "1:395991295721:web:ac8dffa15311f88e9bf278"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
